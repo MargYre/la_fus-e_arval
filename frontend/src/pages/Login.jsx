e5ef1eb8672238c3e78fd69bcfbf06f85a113dc9
margaux <margaux.tarayre@gmail.com>
2024-08-06 14:03:15 +0200

    feat: Add registration link and CSS styling for form-footer in login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,16 @@
+// Dans pages/Login.jsx
 import Form from "../components/form";
+import { Link } from "react-router-dom";
 
 function Login() {   
-    return <Form route="/api/token/" method="login" />
+    return <div>
+        <Form route="/api/token/" method="login" />
+        <div className="form-footer">
+        <p>
+          Don't have an account? <Link to="/register">Register here</Link>
+        </p>
+      </div>
+    </div>
 }
 
 export default Login;
