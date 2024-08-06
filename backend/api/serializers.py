322d731ab55af19222bbaa6c623981067a887420
margaux <margaux.tarayre@gmail.com>
2024-08-06 13:48:40 +0200

    fix: Correct password hashing and authentication setup

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -8,9 +8,9 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
-        def create(self, validated_data):
-            password = validated_data.pop('password')
-            user = User.objects.create_user(**validated_data)
-            user.set_password(password)  # Hash the password
-            user.save()
-            return user
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            password=validated_data["password"]
+        )
+        return user
