862e66e9cbc71c2ba03fd33280f81d67cda8a1b7
margaux <margaux.tarayre@gmail.com>
2024-08-06 12:07:12 +0200

    feat: add login and registration forms

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,7 @@
 function NotFound() {
-    return <div>Not Found</div>
+    return <div>Not Found
+        <p>The page you are looking for does not exist.</p>
+    </div>
 }
 
 export default NotFound;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,8 @@
+// Dans pages/Register.jsx
+import Form from "../components/form";
+
 function Register() {
-    return <div>Register</div>
+    return <Form route="/api/user/register/" method="register" />
 }
 
 export default Register;
