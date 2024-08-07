4b34c2e2a68b6b1114530df2f1004f353e2f337c
margaux <margaux.tarayre@gmail.com>
2024-08-07 19:44:27 +0200

    feat: add InformationBox component with styled green background that list information about reservation

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -2,21 +2,37 @@
 import React from 'react';
 import '../styles/Hero.scss';
 import heroImage from '../assets/Hero.jpeg';
+import electricIcon from '../assets/picto-electrique.png'
 
 const Hero = () => {
   return (
-    <div >
+    <div className='wrapper'>
       <div className="image-container">
         <img src={heroImage} alt="Description" className="heroImage" />
       </div>
       <div className="hero-wrapper">
-      <div className="heroContainer">
-        <h1>LE DUO ÉLECTRIQUE-THERMIQUE ARVAL SWITCH : MODE D’EMPLOI</h1>
-        <p>Avec Arval Switch, vos collaborateurs gagnent en autonomie sur la route, mais aussi pour réserver leur véhicule !<br/>
-        Aucune action n’est requise de votre part : l’ensemble de la prestation est géré de bout en bout par Arval, 
-        en direct avec les conducteurs.</p>
+        <div className="heroContainer">
+          <h1>LE DUO ÉLECTRIQUE-THERMIQUE ARVAL SWITCH : MODE D’EMPLOI</h1>
+          <p>Avec Arval Switch, vos collaborateurs gagnent en autonomie sur la route, mais aussi pour réserver leur véhicule !<br/>
+          Aucune action n’est requise de votre part : l’ensemble de la prestation est géré de bout en bout par Arval, 
+          en direct avec les conducteurs.</p>
+        </div>
       </div>
-      </div>
+      <div className="info-box">
+          <p>
+            1. Sur simple appel téléphonique, ils demandent la mise à disposition d’un véhicule
+            thermique ou hybride, en précisant la catégorie souhaitée.<br />
+            2. Le véhicule est réservé rapidement, dans l’une des 1 400 agences du réseau
+            partenaire d’Arval, réparties dans toute la France métropolitaine (dont Corse).<br />
+            3. Un SMS et/ou un mail de confirmation leur est envoyé, où sont indiqués :
+            <ul>
+              <li>la période de réservation</li>
+              <li>les coordonnées de l’agence où retirer le véhicule</li>
+              <li>le solde restant sur leur réserve annuelle de 20 jours</li>
+            </ul>
+          </p>
+          <img src={electricIcon} alt="Electric Icon" className="electric-icon" />
+        </div>
     </div>
   );
 };
